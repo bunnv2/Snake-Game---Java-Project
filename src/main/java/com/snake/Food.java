6d3324ec6884dea48444060fc6279a931b9c45ef
Wojciech Królik <bylic22@gmail.com>
2022-01-22 23:43:03 +0100

    Food spawn and moving logic

--- src/main/java/com/snake/Food.java
+++ src/main/java/com/snake/Food.java
@@ -1,5 +1,32 @@
 package com.snake;
 
-public class Food {
+import java.awt.Rectangle;
 
+public class Food {
+    private int x;
+    private int y;
+
+    public Food(Snake snake) {
+        this.randomize(snake);
+    }
+
+    public void randomize(Snake snake) {
+        x = (int) (Math.random() * Game.WIDTH);
+        y = (int) (Math.random() * Game.HEIGHT);
+        boolean collision = false;
+
+        while (!collision) {
+            for (Rectangle rect : snake.getSnake()) {
+                if (rect.getX() == x * Game.SCALE && rect.getY() == y * Game.SCALE) {
+                    collision = true;
+                }
+            }
+
+            if (collision) {
+                x = (int) (Math.random() * Game.WIDTH);
+                y = (int) (Math.random() * Game.HEIGHT);
+                collision = false;
+            }
+        }
+    }
 }
--- src/main/java/com/snake/Snake.java
+++ src/main/java/com/snake/Snake.java
@@ -2,6 +2,7 @@ package com.snake;
 
 import java.util.*;
 import java.awt.*;
+import javax.swing.*;
 
 public class Snake {
     private ArrayList<Rectangle> snake;
@@ -24,6 +25,43 @@ public class Snake {
         snake.add(temp);
 
         direction = "NONE";
+    }
+
+    public void move() {
+        if (direction != "NONE") {
+            Rectangle temp = new Rectangle(Game.SCALE, Game.SCALE);
+            Rectangle head = snake.get(0);
+
+            if (direction == "UP") {
+                temp.setLocation(head.x, head.y - Game.SCALE);
+            } else if (direction == "DOWN") {
+                temp.setLocation(head.x, head.y + Game.SCALE);
+            } else if (direction == "LEFT") {
+                temp.setLocation(head.x - Game.SCALE, head.y);
+            } else if (direction == "RIGHT") {
+                temp.setLocation(head.x + Game.SCALE, head.y);
+            }
+
+            snake.add(0, temp);
+            snake.remove(snake.size() - 1);
+        }
+    }
+
+    public void grow() {
+        Rectangle temp = new Rectangle(Game.SCALE, Game.SCALE);
+        Rectangle head = snake.get(0);
+
+        if (direction == "UP") {
+            temp.setLocation(head.x, head.y - Game.SCALE);
+        } else if (direction == "DOWN") {
+            temp.setLocation(head.x, head.y + Game.SCALE);
+        } else if (direction == "LEFT") {
+            temp.setLocation(head.x - Game.SCALE, head.y);
+        } else if (direction == "RIGHT") {
+            temp.setLocation(head.x + Game.SCALE, head.y);
+        }
+
+        snake.add(0, temp);
     }
 
     public ArrayList<Rectangle> getSnake() {
